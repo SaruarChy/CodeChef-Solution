762535fadc5de174bca1cc9490c935754c694ac3
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-10-18 00:36:24 +0600

    Add files via upload

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,a,b,i,sum;
+    cin>>t;
+    for(i=0; i<t; i++)
+    {
+        cin>>a>>b;
+        sum=a+b;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
